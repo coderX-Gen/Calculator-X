71342cb1d527678054e680399544da42bcafa166
coderX-Gen <kotlimanvir2007@gmail.com>
2025-02-01 22:36:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let display = document.getElementById('result');
+
+//Appends the clicked buutons value in display
+function append(value) {
+    display.value += value;
+}
+
+//Clear the calcultor display
+function clearDisplay() {
+    display.value = '';
+}
+
+//Squares the current value in display
+function square() {
+    try { 
+    display.value = Math.pow(parseFloat(display.value), 2);
+}   catch (error){
+    display.value='Error';
+}
+}
+
+function calculate() {
+    try{let expression = display.value.replace(/(\d+)%/g,'($1/100)');
+        display.value= eval(expression);
+    } catch (error) {
+        display.value='Error';
+    }
+
+}
+
+function backspace(){
+    display.value =
+    display.value.slice(0,-1);
+}
+
+function squareRoot() {
+    try{
+        display.value = Math.sqrt(parseFloat(display.value));
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+/*Evaluate the expression  in the display
+function calculate() {
+    try {
+        //use eval to evaluate the expression entered by user
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}*/
